c16d3213ec776513c37255d982717bbec0ac8f59
sproctor291 <stephenproctor291@gmail.com>
2022-11-19 13:48:49 -0600

    A main function has appeared!

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import mysql.connector
+
+
+def main():
+    print("\nWelcome to the E-Commerce Store!")
+    print("Version 1.0.0 by Group 10\n")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
