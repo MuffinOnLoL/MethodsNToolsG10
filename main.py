d4d6f3a72a54f24b262204d01bcb31930ca5098f
sproctor291 <stephenproctor291@gmail.com>
2022-11-19 22:44:37 -0600

    SQL connected

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import mysql.connector
 
 
-def main():
-    print("\nWelcome to the E-Commerce Store!")
-    print("Version 1.0.0 by Group 10\n")
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    database = "ECommerce_Group10"
+)
+
+mycursor = mydb.cursor()
+
+#class account:
+
+
+#class shoppingCart:
+
+
+##class orderHistory:
+    ##def __init__(self, purchased, shippingInfo, paymentInfo):
+        ##self.purchased = purchased
+        #self.shippingInfo = shippingInfo
+        #self.paymentInfo = paymentInfo
+
+    ##def addToOrderHistory(self):
 
 
 
-if __name__ == "__main__":
-    main()
+
+#class inventory:
+
+
+##def main():
+    ##print("\nWelcome to the E-Commerce Store!")
+    ##print("Version 1.0.0 by Group 10\n")
+
+
+
+##if __name__ == "__main__":
+    ##main()
 
 
